adb90c8af8cd693a2c2338b408df968af88f9163
hariharan-2407 <hariharanthangarasu@gmail.com>
2026-03-31 09:41:02 +0530

    Initial clean commit - Airflow project

---
+++ DAGS/4_XCOMs_Auto.py
@@ -0,0 +1,37 @@
+from airflow.sdk import dag, task 
+
+
+@dag(
+        dag_id="xcoms_dag_auto",
+)
+def xcoms_dag_auto():
+
+    @task.python
+    def first_task():
+        print("Extracting Data,....This is the first task")
+        fetched_data={"data",[1,2,3,4,5]}
+        return fetched_data
+
+    @task.python
+    def second_task(data:dict):
+        fetched_data = data['data']
+        transformed_data = fetched_data *2
+        transformed_data_dict = {'trans_dict':transformed_data}
+        return transformed_data_dict
+        print("Transformed Data,....This is the second task")
+    
+    @task.python
+    def third_task(data:dict):
+        load_data = data['data']
+
+    
+    
+    # Defining task dependencies
+    first = first_task()
+    second = second_task(first)
+    third = third_task(second)
+    
+    first >> second >> third
+
+# Instantiating the DAG
+xcoms_dag_auto()
---
+++ DAGS/5_XCOMs_Kwargs.py
@@ -0,0 +1,49 @@
+from airflow.sdk import dag, task 
+
+
+@dag(
+        dag_id="xcoms_dag_kwargs",
+)
+def xcoms_dag_kwargs():
+
+    @task.python
+    def first_task(**kwargs):
+
+        ti = kwargs['ti']
+
+        print("Extracting data... This is the first task")
+        fetched_data = {"data": [1, 2, 3, 4, 5]}
+        ti.xcom_push(key='return_result',value=fetched_data)
+
+
+    @task.python
+    def second_task(**kwargs):
+        ti= kwargs['ti']
+
+        fetched_data=  ti.xcom_pull(task_ids='first_task', key='return_result')
+        data = fetched_data["data"]   # extract list
+
+        transformed_data = [x * 2 for x in data]
+        
+
+        ti.xcom_push(key='return_result',value=transformed_data)
+
+
+    @task.python
+    def third_task(**kwargs):
+       ti = kwargs['ti']
+
+       load_data = ti.xcom_pull(task_ids='second_task', key='return_result')
+
+       return load_data
+        
+    
+    # Defining task dependencies
+    first = first_task()
+    second = second_task()
+    third = third_task()
+    
+    first >> second >> third
+
+# Instantiating the DAG
+xcoms_dag_kwargs()
